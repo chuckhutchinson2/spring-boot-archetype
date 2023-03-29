c6d6da81fcd75cb60b8e8dcb83496bfc017864f1
chuck hutchinson <chuckhutchinson2@yahoo.com>
2023-03-29 15:28:52 -0400

    update

---
+++ create.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mvn archetype:generate -DarchetypeGroupId=org.example -DarchetypeArtifactId=springboot-archetype -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=org.garden.home -DartifactId=RaisedBedGarden -Dversion=1.0-SNAPSHOT
