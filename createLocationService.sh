2af069e9d8fe7a00a5cd5e01fde8812c0fdda14c
chuck hutchinson <chuckhutchinson2@yahoo.com>
2023-03-31 13:55:51 -0400

    updated resource fileset

---
+++ createLocationService.sh
@@ -0,0 +1,12 @@
+mvn archetype:generate -DarchetypeGroupId=org.example \
+ -DarchetypeArtifactId=springboot-archetype \
+ -DarchetypeVersion=1.0-SNAPSHOT \
+ -DgroupId=org.example.location \
+ -DartifactId=LocationServices \
+ -Dversion=1.0-SNAPSHOT \
+ -DmodelClass=Location \
+ -Dname=LocationServices \
+ -Ddescription='Location Location that provides US city, state, zip, lat, long location data' \
+ -DrestEndpoint=/api/v1/location \
+ -DserviceName=Location
+
